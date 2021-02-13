678274eeeffcfb4d6ac3d9e0796cc65802402352
jenpoyueh <jenpoyueh@simbo.com.tw>
2021-02-14 02:12:00 +0800

    game001

---
+++ cocos/game/game001/assets/src/data/MyData.ts
@@ -0,0 +1,7 @@
+/**
+ * 模型层
+ * 数据实体类
+ */
+export default class MyData {
+    public Level: number = 0;
+}
---
+++ cocos/game/game001/assets/src/proxy/DataProxy.ts
@@ -0,0 +1,29 @@
+
+import MyData from "../data/MyData"
+/**
+ * 模型层
+ * 数据代理类
+ */
+export default class DataProxy extends puremvc.Proxy {
+    public proxyName = "DataProxy";
+    private MyData: MyData = null;
+    private static instance: DataProxy = null;
+ 
+    public static getInstance() {
+        if(!this.instance) {
+            this.instance = new DataProxy();
+        }
+        return this.instance;
+    }
+ 
+    constructor() {
+        super();
+        puremvc.Proxy.NAME = "DataProxy";
+        this.MyData = new MyData();
+    }
+ 
+    public AddLevel(addNumber: number) {
+        this.MyData.Level += addNumber;
+        this.sendNotification("Msg_AddLevel", this.MyData);
+    }
+}
